d1efe7e3fa401552caa42feb49eac2d3679303dc
carolainseija <carolainsilva1@gmail.com>
2022-03-11 12:14:57 -0300

    example env local

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const apikey= process.env.REACT_APP_GOOGLE_KEY;
+
+const example EnvLocal = () => {
+    return(
+        <>
+        </>
+    )
+}
+export default EnvLocal;
